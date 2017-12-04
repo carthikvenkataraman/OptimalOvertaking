26e8bcad2ce7199ba95ba20d06293ca7cf34e11f
Karthik Venkataraman <karthik.venkataraman@zenuity.com>
2017-12-04 21:21:03 +0100

    Fixes errors that caused NaNs in earlier implementations' vEx

--- Code/UpdateScenario.m
+++ Code/UpdateScenario.m
@@ -8,13 +8,13 @@ updTask.E.y0  = decision.yE(2);
 updTask.E.vx0 = decision.vEx(2);
 updTask.E.vy0 = decision.vEy(2);
 
-updTask.L.x0    = updTask.L.x0 + (updTask.L.vx - decision.vEx(2))*dt;
+updTask.L.x0    = updTask.L.x0 + updTask.L.vx*dt;
 
 if updTask.adjacentveh
-    updTask.A.x0 = updTask.A.x0 + (updTask.A.vx - decision.vEx(2))*dt;
+    updTask.A.x0 = updTask.A.x0 + updTask.A.vx*dt;
 end
 
 if updTask.adjacentveh2
-    updTask.A2.x0 = updTask.A2.x0 + (updTask.A2.vx - decision.vEx(2))*dt;
+    updTask.A2.x0 = updTask.A2.x0 + updTask.A2.vx*dt;
 end
 end
